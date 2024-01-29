771323798e0501e50b560211982f9a2420f7e1d1
saiful <saiful0687@gmail.com>
2024-01-29 18:58:48 -0500

    todo UI complete

---
+++ lib/todo_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TodoApp extends StatefulWidget {
+  const TodoApp({Key? key}) : super(key: key);
+
+  @override
+  State<TodoApp> createState() => _TodoAppState();
+}
+
+class _TodoAppState extends State<TodoApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
---
+++ lib/todo_list.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class TodoList extends StatefulWidget {
+  const TodoList({Key? key}) : super(key: key);
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView.separated(
+        itemCount: 10,
+        itemBuilder: (context,index){
+            return ListTile(
+              title: Text("Todo Title"),
+              subtitle:Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+
+                  Text("Todo Body"),
+                  Text("Time")
+                ],
+              ),
+              trailing: Wrap(
+                children: [
+                  IconButton(onPressed: (){}, icon: Icon(Icons.edit)),
+                  IconButton(onPressed: (){}, icon: Icon(Icons.delete))
+                ],
+              ),
+            );
+          },
+
+
+          separatorBuilder: (context,index){
+            return Divider();
+          }, )
+    );
+  }
+}
